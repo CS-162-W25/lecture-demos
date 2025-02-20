526a6c6bdb8cb3cf6fbeb8010105c094fabdff0c
Alexander Guyer <guyera@oregonstate.edu>
2025-02-20 13:56:49 -0800

    2/19 lecture

--- polymorphism/main.cpp
+++ polymorphism/main.cpp
@@ -3,16 +3,20 @@
 #include "vampire.hpp"
 
 void do_monster_turns(
-		zombie* zombies,
-		int n_zombies,
-		vampire* vampires,
-		int n_vampires) {
-	for (int i = 0; i < n_zombies; i++) {
-		zombies[i].do_turn();
-	}
+		monster** monsters,
+		int n_monsters) {
+	for (int i = 0; i < n_monsters; i++) {
+		// Static binding
+		// "Static": "At compile time"
+		// "Dyanmic": "At runtime"
+		monsters[i]->do_turn();
 
-	for (int i = 0; i < n_vampires; i++) {
-		vampires[i].do_turn();
+		// Every variable has a static type and a dynamic type
+		// Static type of a variable: Its declared type
+		// Dynamic type of a variable: Its "actual" type
+		//
+		// Static type monsters[i]: monster*
+		// Dynamic type monsters[i]: either vampire* or zombie*
 	}
 }
 
@@ -21,10 +25,20 @@ int main() {
 	// m.do_turn();
 
 	// This calls the default zombie constructor
-	zombie* z = new zombie[5];
-	vampire* v = new vampire[10];
+	// zombie* z = new zombie[5];
+	// vampire* v = new vampire[10];
+	monster** monsters = new monster*[15];
+	for (int i = 0; i < 5; i++) {
+		zombie* z = new zombie;
+		monsters[i] = z;
+	}
+	for (int i = 5; i < 15; i++) {
+		vampire* v = new vampire;
+		monsters[i] = v;
+	}
+
 	
-	do_monster_turns(z, 5, v, 10);
-	do_monster_turns(z, 5, v, 10);
-	do_monster_turns(z, 5, v, 10);
+	do_monster_turns(monsters, 15);
+	do_monster_turns(monsters, 15);
+	do_monster_turns(monsters, 15);
 }
